c4f4ea7ce9ae6b044494dc27cc0316bd91180628
SumeyraGnl <h.berengunel@gmail.com>
2023-05-18 09:50:42 +0300

    18.05.2023

---
+++ src/test/java/tests/day14_TestNGFrameworkOlusturma/C03_DependsOnMethods.java
@@ -0,0 +1,40 @@
+package tests.day14_TestNGFrameworkOlusturma;
+
+import org.testng.annotations.Test;
+
+public class C03_DependsOnMethods {
+
+    /*
+
+        DependsOnmethods oncelik sirasina karismaz
+
+        dependsOnMethods bir test method'un calismasini
+        baska bir method'un calisip passed olmasina baglar
+
+        baglanilan method calismaz veya calisir ama failed olursa
+        baglanan method hic calismaz
+
+
+
+
+     */
+
+    @Test(dependsOnMethods = "wiseTesti")
+    public void youtubeTesti(){
+
+        System.out.println("Youtube");
+
+    }
+    @Test(priority = 3)
+    public void wiseTesti(){
+
+        System.out.println("Wise Quarter");
+
+    }
+    @Test(dependsOnMethods = "youtubeTesti")
+    public void amazonTesti(){
+
+        System.out.println("Amazon");
+
+    }
+}

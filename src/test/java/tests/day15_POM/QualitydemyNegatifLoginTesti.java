d45ea0d83f8a28ced6ca7c52f5ff8621d201b98e
SumeyraGnl <h.berengunel@gmail.com>
2023-05-19 15:07:40 +0300

    19.05.2023

---
+++ src/test/java/tests/day15_POM/QualitydemyNegatifLoginTesti.java
@@ -0,0 +1,24 @@
+package tests.day15_POM;
+
+import org.testng.annotations.Test;
+
+public class QualitydemyNegatifLoginTesti {
+
+    @Test
+    public void test01(){
+        // QUALITYDEMY ANA SAYFAYA GIDIN
+
+        // login linkine tiklayin
+
+        // gecerli username ve sifre yi ilgili kutulara yazin
+
+
+        // login butonuna basin
+
+
+        // basarili olarak giris yapildigini test edin
+
+        // sayfayi kapatin
+    }
+
+}

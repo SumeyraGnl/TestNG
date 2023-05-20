c9e03548c7e780c42100cd760b572759e79460c6
SumeyraGnl <h.berengunel@gmail.com>
2023-05-20 09:29:32 +0300

    20.05.2023

--- src/test/java/tests/day15_POM/QualitydemyNegatifLoginTesti.java
+++ src/test/java/tests/day15_POM/QualitydemyNegatifLoginTesti.java
@@ -1,6 +1,10 @@
 package tests.day15_POM;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 
 public class QualitydemyNegatifLoginTesti {
 
@@ -8,17 +12,30 @@ public class QualitydemyNegatifLoginTesti {
     public void test01(){
         // QUALITYDEMY ANA SAYFAYA GIDIN
 
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+
         // login linkine tiklayin
 
-        // gecerli username ve sifre yi ilgili kutulara yazin
+        QualitydemyPage qualitydemyPage=new QualitydemyPage();
+        qualitydemyPage.ilkLoginLinki.click();
+
+        // gecersiz username ve sifre yi ilgili kutulara yazin
+
+        qualitydemyPage.emailKutusu.sendKeys("qdGecersizUsername");
+        qualitydemyPage.passwordKutusu.sendKeys("qdGecersizPassword");
 
 
         // login butonuna basin
 
+        qualitydemyPage.loginButonu.click();
 
-        // basarili olarak giris yapildigini test edin
+        // basarili olarak giris yapilamadigini test edin
+
+        Assert.assertTrue(qualitydemyPage.emailKutusu.isEnabled());
 
         // sayfayi kapatin
+
+        Driver.getDriver().close();
     }
 
 }
--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,5 +1,8 @@
 package utilities;
 
+import com.beust.ah.A;
+import org.openqa.selenium.interactions.Actions;
+
 public class ReusableMethods {
 
     public static void bekle(int saniye) {
@@ -10,4 +13,5 @@ public class ReusableMethods {
 
         }
     }
+
 }
